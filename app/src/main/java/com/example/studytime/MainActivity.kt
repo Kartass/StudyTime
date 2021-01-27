64737fb153b416ad2c2795b4ec61a672987f4c48
Kartass <jw5150@naver.com>
2021-01-28 00:28:05 +0900

    create login fragment

--- app/src/main/java/com/example/studytime/MainActivity.kt
+++ app/src/main/java/com/example/studytime/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.studytime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.example.studytime.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHost) as NavHostFragment
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 }
